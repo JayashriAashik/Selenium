82b368b8024f855047587534199cf5ec1263080a
JayashriAashik <jaishri.sridharan@gmail.com>
2025-01-05 12:46:54 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,23 @@
+package week2.day2;
+
+public class Library {
+
+	public String addBook(String bookTitle)
+	{
+		System.out.println("Book added successfully");
+		return "bookTitle";
+		
+	}
+	
+    public void issueBook() {
+		System.out.println("Book issued successfully");
+
+    }
+	public static void main(String[] args) {
+	Library lib=new Library();
+	lib.addBook("ABCD");
+	lib.issueBook();
+	
+	}
+
+}
---
+++ LibraryManagement.java
@@ -0,0 +1,11 @@
+package week2.day2;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		Library l=new Library();
+		l.issueBook();
+		l.addBook("WXYZ");
+	}
+
+}
